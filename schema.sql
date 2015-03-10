e5e9cc07e6bc71f88bce97caa8b3b0ada19a5ece
lucasstanesa <defitl@yahoo.com>
2015-03-10 11:31:55 -0400

    Created simple SQL database schema

---
+++ schema.sql
@@ -0,0 +1,21 @@
+--   Simple book schema
+--
+--   FIELDS
+--
+--   id - The row id
+--   edition - The release edition of the book
+--   author - The person(s) who wrote the book
+--   isbn - The International Standard Book Number for the book, should be stripped of '-' dashes
+--   publisher - The individual or company that published the book
+--   pageCount - How many pages long the book is
+--
+
+CREATE TABLE Book (
+    `id` INT AUTO_INCREMENT,
+    `edition` VARCHAR(20),
+    `author` VARCHAR(50),
+    `isbn` VARCHAR(13),
+    `publisher` VARCHAR(50),
+    `pageCount` INT,
+    PRIMARY KEY(id)
+);
