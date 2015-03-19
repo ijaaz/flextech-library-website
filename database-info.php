4d55decfbdec1f1ef664a32ca3ed43e81a74b07a
AfroChikken <bterova@flextech-hs.org>
2015-03-19 11:27:24 -0400

    Setting up PHP database input.
    Added a way to easily include database information, as well as creating
    a method of which people can put in book/event information. Still not
    complete.

---
+++ database-info.php
@@ -0,0 +1,7 @@
+<?php
+//Use this in any document you're going to be accessing a database in, using include("databas-info.php");
+$SQL_Username = "";
+$SQL_Password = "";
+$SQL_Database = "";
+//Not going to be including Table, as it varies from section to section.
+?>
