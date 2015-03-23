7650d39eb824517db0c8e8ec100c9755741c8f08
Blatherskite <osamala@flextech-hs.org>
2015-03-23 11:30:31 -0400

    Mass fix
    Let this be a lesson to do pull requests if you are behind.

--- books.php
+++ books.php
@@ -10,15 +10,14 @@
     <meta charset="UTF-8">
     <title>Flextech Online Library</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta name="description" content="The Flextech Online Library" />
-    <meta name="keywords" content="flextech, online, library" />
-    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
     <script src="js/jquery.min.js"></script>
     <script src="js/skel.min.js"></script>
     <script src="js/skel-layers.min.js"></script>
     <script src="js/init.js"></script>
     <link rel="stylesheet" href="css/skel.css" />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/browse_style.css" />
     <link rel="stylesheet" href="css/style-xlarge.css" />
 </head>
 
@@ -26,27 +25,26 @@
 
     <!-- Header -->
     <header id="header">
-        <h1><a href="index.html"><img src="images/flextech-logo.png" width="185px" height="70px"/></a></h1>
+        <h1><a href="index.html"><img src="images/flextech-logo.png" width="170px" height="70px"/></a></h1>
 
         <nav id="nav">
             <ul>
-                <li><a href="index.html">Home</a>
+                <li><a href="index.html"><font color="#333">Home</font></a>
                 </li>
-                <li><a href="browse.html" class="button-special">Browse</a>
+                <li><a href="browse.html"><font color="#333">Browse</font></a>
                 </li>
             </ul>
         </nav>
     </header>
 
-    <!-- Banner -->
-    <section id="banner">
-        <h2>FlexTech Online Library</h2>
-        <p>Welcome to the Flextech Online Library!</p>
-        <ul class="actions">
-            <li>
-                <a href="browse.html" class="button big">Browse Library</a>
-            </li>
-        </ul>
+    <section id="one" class="wrapper style1 special">
+        <div class="container">
+            <header class="major">
+                <h2>Browse the Book Library</h2>
+                <p>Look at all these amazing books that we slaved to get...</p>
+            </header>
+            
+        </div>
     </section>
 
 </body>
